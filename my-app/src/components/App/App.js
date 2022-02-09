1a447d92a0b0972774331291e151e56a36c75964
Anita Caira Percival <anita.percival1@gmail.com>
2022-02-09 14:36:49 +0000

    Game over function

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -6,15 +6,31 @@ function App() {
 
   const [board, setBoard] = useState(["O", "X", null, null, null, null, null, null, null,])
   const [playerOne,setPlayerOne] = useState(true)
+  
   function makeAMove(index) {
+
     const playerMove = playerOne ? "X" : "O";
     setBoard([...board.slice(0, index), playerMove, ...board.slice(index + 1)])
     setPlayerOne(!playerOne);
+    
+
   }
+
+    function gameOver() {
+      if (!board.includes(null)) {
+        return "Game Over"
+      }
+    }
+
+  
+
 console.log(playerOne);
   return (
     <div>
+        <h1 className="gameOver">{gameOver()}</h1>
+        <p className="whosTurn">{playerOne ? "Player 1's Turn" : "Player 2's Turn"}</p>
         <Board board={board} makeAMove={makeAMove} ></Board>
+        
     </div>
     
   );
