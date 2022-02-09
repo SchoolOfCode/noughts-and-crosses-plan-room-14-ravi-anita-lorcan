36766356b78ba19c4b35f8b64e61990793b8b43e
Anita Caira Percival <anita.percival1@gmail.com>
2022-02-09 12:31:26 +0000

    board css

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -1,10 +1,14 @@
 import Board from './../Board/index';
 import './App.css';
+import { useState } from "react"
 
 function App() {
+
+  const [board, setBoard] = useState([null, null, null, null, null, null, null, null, null,])
+
   return (
     <div>
-        <Board />
+        <Board board={board}></Board>
     </div>
     
   );
--- my-app/src/components/Board/index.js
+++ my-app/src/components/Board/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Square from '../Square'
+import "./index.css"
+
 const Board = () => {
   return (
-    <div>
+    <div className="board">
         <Square />
     </div>
   )
