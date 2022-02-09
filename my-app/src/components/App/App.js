81f0ec1055a567ca319229230ad964d4c228d9b0
ravigordon <rgordz460@outlook.com>
2022-02-09 12:15:22 +0000

    Board folder and Square folder created

---
+++ my-app/src/components/App/App.js
@@ -0,0 +1,13 @@
+import Board from './../Board/index';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <Board />
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Square/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Square = () => {
+  return (
+    <div>
+        <p>Square</p>
+    </div>
+  )
+}
+
+export default Square
